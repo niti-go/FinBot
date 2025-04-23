a93a86cf7eff5a933364ad26d197dc6706bcf39e
Allison Jin <allison.yq.jin@gmail.com>
2025-04-23 06:42:26 -0400

    integrated app.py with prompt.py and simple embellishments

--- prompt.py
+++ prompt.py
@@ -10,32 +10,33 @@ import pandas as pd
 import os
 import config # Put your API key in config.py
 
-GEMINI_API_KEY = config.GEMINI_API_KEY
-os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
+def init_agent(): 
+    GEMINI_API_KEY = config.GEMINI_API_KEY
+    os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
 
-#genai.configure(api_key=GEMINI_API_KEY)
-# for m in genai.list_models():
-#     print(m.name, "-", m.supported_generation_methods)
+    #genai.configure(api_key=GEMINI_API_KEY)
+    # for m in genai.list_models():
+    #     print(m.name, "-", m.supported_generation_methods)
 
-#llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo", temperature=0)
+    #llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo", temperature=0)
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", google_api_key=GEMINI_API_KEY, temperature=0)
-df = pd.read_csv("13f_filings.csv")
-conn = sqlite3.connect("testdatabase.db")
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", google_api_key=GEMINI_API_KEY, temperature=0)
+    df = pd.read_csv("13f_filings.csv")
+    conn = sqlite3.connect("testdatabase.db")
 
-# Convert DataFrame to a SQLite table named "Filings"
-df.to_sql("Filings", conn, if_exists='replace')
-my_db = SQLDatabase.from_uri("sqlite:///testdatabase.db")
+    # Convert DataFrame to a SQLite table named "Filings"
+    df.to_sql("Filings", conn, if_exists='replace')
+    my_db = SQLDatabase.from_uri("sqlite:///testdatabase.db")
 
-agent_executor = create_sql_agent(
-    llm=llm,
-    toolkit=SQLDatabaseToolkit(db=my_db, llm=llm),
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    handle_parsing_errors=True,
-    verbose=True,
-    return_intermediate_steps=True
-)
+    agent_executor = create_sql_agent(
+        llm=llm,
+        toolkit=SQLDatabaseToolkit(db=my_db, llm=llm),
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True,
+        verbose=True,
+        agent_executor_kwargs={"return_intermediate_steps": True}
+    )
 
-user_inquiry = "What major institutions are included in these filings and what makes them significant?"
-
-agent_executor.invoke(user_inquiry)
+    return agent_executor, df
+    # user_inquiry = "What major institutions are included in these filings and what makes them significant?"
+    #agent_executor.invoke(user_inquiry)
